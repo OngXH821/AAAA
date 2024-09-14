8dd5071d9a5a61accfcb419bd2b4cc9cd2cbff95
OngXH821 <ongxh-jm22@student.tarc.edu.my>
2024-09-15 01:29:37 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,18 @@
+import os
+import subprocess
+import sys
+
+# Function to install packages
+def install(package):
+    subprocess.check_call([sys.executable, "-m", "pip", "install", package])
+
+# Install nltk if it's not already installed
+try:
+    import nltk
+except ImportError:
+    install("nltk")
+    import nltk
+
 import re
 import pandas as pd
 import nltk
